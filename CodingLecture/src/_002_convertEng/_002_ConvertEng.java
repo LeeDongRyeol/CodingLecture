532d8a0ffff25a539aa17cef969660f3d4e2d05b
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2021-08-29 00:45:07 +0900

    대소문자 변환 아스키코드로 풀이

--- CodingLecture/src/_002_convertEng/_002_ConvertEng.java
+++ CodingLecture/src/_002_convertEng/_002_ConvertEng.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
  *
  *	2강 . 대소문자 변환
  *
- * 어설프게 아스키코드 쓰지 말것.
+ *	아스키코드
+ *	대문자 : 65 ~ 90
+ *	소문자 : 97 ~ 122
  *
  */
 
@@ -25,8 +27,12 @@ public class _002_ConvertEng {
 		String answer = "";
 		
 		for(char x : str.toCharArray()) {
-			if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
-			else answer += Character.toLowerCase(x);
+//			if(Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+//			else answer += Character.toLowerCase(x);
+			
+			if(x >= 97 && x <= 122) answer += (char)(x-32);
+			else answer += (char)(x+32);
+			
 		}
 		
 		return answer;
