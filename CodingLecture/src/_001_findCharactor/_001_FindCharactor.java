54c5966f58a1a20414a920e9cc36fbfd613ee8f8
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2021-08-28 23:29:28 +0900

    커밋 왜 안됌?

--- CodingLecture/src/_001_findCharactor/_001_FindCharactor.java
+++ CodingLecture/src/_001_findCharactor/_001_FindCharactor.java
@@ -4,16 +4,37 @@ import java.util.Scanner;
 
 public class _001_FindCharactor {
 	public static void main(String[] args) {
+		_001_FindCharactor m = new _001_FindCharactor();
 		Scanner sc = new Scanner(System.in);
 		
 		String str = sc.nextLine().toLowerCase();
 		String ch = sc.next().toLowerCase();
 		
-		int cnt = 0;
-		for (int i = 0; i < str.length(); i++) {
-			if(ch.equals(str.substring(i, i+1))) cnt++;
+		System.out.println(m.solution(str, ch.charAt(0)));
+		
+		
+//		int cnt = 0;
+//		for (int i = 0; i < str.length(); i++) {
+//			if(ch.equals(str.substring(i, i+1))) cnt++;
+//		}
+//		
+//		System.out.println(cnt);
+	}
+	
+	public static int solution(String str, char t) {
+		int answer = 0;
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);
+		
+//		for (int i = 0; i < str.length(); i++) {
+//			if(str.charAt(i) == t) answer++;
+//		}
+		
+		for(char x : str.toCharArray()) {
+			if(x == t) answer++;
 		}
 		
-		System.out.println(cnt);
+		
+		return answer;
 	}
 }
