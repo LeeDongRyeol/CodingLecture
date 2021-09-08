132c361a0ab65b50767de0beecccdb85d345c455
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2021-09-08 11:04:40 +0900

    매출액의 종류

--- CodingLecture/src/_04_mapSet/_003_typeOfTake/Main.java
+++ CodingLecture/src/_04_mapSet/_003_typeOfTake/Main.java
@@ -26,21 +26,17 @@ public class Main {
 		List<Integer> list = new ArrayList<Integer>();
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 		
-		int lt = 0;
+		for (int i = 0; i < k-1; i++) {
+			map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+		}
 		
-		for (int rt = 0; rt < n; rt++) {
-			if(k <= rt) {
-				map.put(arr[lt], map.get(arr[lt]) - 1);
-				if(map.get(arr[lt]) == 0) {
-					map.remove(arr[lt++]);
-				}
-			}
-			
+		int lt = 0;		
+		for (int rt = k-1; rt < n; rt++) {
 			map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
-			
-			if(k - 1 <= rt) {
-				list.add(map.size());
-			}
+			list.add(map.size());
+			map.put(arr[lt], map.get(arr[lt]) -1);
+			if(map.get(arr[lt]) == 0) map.remove(arr[lt]);
+			lt++;
 		}
 		
 		return list.stream().mapToInt(Integer::intValue).toArray();
