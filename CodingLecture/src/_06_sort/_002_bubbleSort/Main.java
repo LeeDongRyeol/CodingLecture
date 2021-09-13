8e17fb3e7092ebeec2c281c7c5f92d8919c857b2
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2021-09-13 15:32:22 +0900

    버블정렬

---
+++ CodingLecture/src/_06_sort/_002_bubbleSort/Main.java
@@ -0,0 +1,30 @@
+package _06_sort._002_bubbleSort;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+		
+		for(int i : T.solution(n, arr)) System.out.print(i + " ");
+	}
+	
+	public static int[] solution(int n, int[] arr) {
+		for (int i = 0; i < n -1; i++) {
+			for (int j = 0; j < n - i -1; j++) {
+				if(arr[j] > arr[j + 1]) {
+					int tmp = arr[i];
+					arr[i] = arr[j + 1];
+					arr[j + 1] = tmp;
+				}
+			}
+		}
+		
+		return arr;
+	}
+}
