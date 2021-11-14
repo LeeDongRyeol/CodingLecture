1614421c5a3e83d198d399ac77c99cf88c72f754
LeeDongRyeol <53504172+LeeDongRyeol@users.noreply.github.com>
2021-11-14 18:16:19 +0900

    최대 점수 구하기

---
+++ CodingLecture/src/_08_dfsBfs/_003_getMaxScore/Main.java
@@ -0,0 +1,36 @@
+package _08_dfsBfs._003_getMaxScore;
+
+import java.util.Scanner;
+
+public class Main {
+	static int answer = Integer.MIN_VALUE, n, m;
+	static boolean flag = false;
+	
+	public void DFS(int L, int sum, int time, int[] ps, int[] pt) {
+		if(time > m) return;
+		if(L==n) {
+			answer = Math.max(sum, answer);
+		} else {
+			DFS(L+1, sum + ps[L], time + pt[L], ps, pt);
+			DFS(L+1, sum , time, ps, pt);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Main T = new Main();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		int[] a = new int[n];
+		int[] b = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+			b[i] = sc.nextInt();
+		}
+		
+		T.DFS(0, 0, 0, a, b);
+		System.out.println(answer);
+	}
+}
